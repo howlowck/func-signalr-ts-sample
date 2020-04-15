bc0f6e1d4de7ba0e4f9b35384dc0d39d62da9cb5
Hao Luo <howlowck@gmail.com>
2020-04-15 12:58:04 -0500

    added context.done

--- negotiate/index.ts
+++ negotiate/index.ts
@@ -2,6 +2,7 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, connectionInfo: any): Promise<void> {
     context.res.body = connectionInfo;
+    context.done();
 };
 
 export default httpTrigger;
